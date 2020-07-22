aead7eaa445b4a84cf8628efad022685feae9a93
plwieseler@gmail.com <plwieseler@gmail.com>
2020-07-22 15:57:16 -0500

    Fixed error that didn't get push

--- TodoWebAPI/UserStories/RoleChanges/RoleChange.cs
+++ TodoWebAPI/UserStories/RoleChanges/RoleChange.cs
@@ -1,9 +1,10 @@
 using System;
 using MediatR;
+using Todo.Domain;
 
 namespace TodoWebAPI.UserStories.RoleChanges
 {
-    public class RoleChange : IRequest
+    public class RoleChange : IRequest<AccountPlan>
     {
         public string Plan {get; set;}
         public Guid AcountId {get; set;}
