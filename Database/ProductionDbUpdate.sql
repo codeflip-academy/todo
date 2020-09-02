25ef62424af1c32bcb41d004bdfa25b52bbf7ec8
plwieseler@gmail.com <plwieseler@gmail.com>
2020-09-02 10:41:49 -0500

    updated db scripts.

--- Database/ProductionDbUpdate.sql
+++ Database/ProductionDbUpdate.sql
@@ -88,13 +88,19 @@ GO
 -- Account.sql
 CREATE TABLE [Accounts]
 (
+
     [ID] UNIQUEIDENTIFIER PRIMARY KEY,
-    [FullName] VARCHAR(50),
-    [PictureUrl] VARCHAR(255),
-    [Email] VARCHAR(50) NOT NULL,
+    [FullName] NVARCHAR(50),
+    [PictureUrl] NVARCHAR(255),
+    [Email] NVARCHAR(50) NOT NULL,
     [CustomerID] VARCHAR(9) NOT NULL,
     [PaymentMethodID] VARCHAR(50),
     [SubscriptionID] VARCHAR(50),
+    [PaymentMethodDeletedPlan] VARCHAR(50),
+    [EmailDueDate] BIT,
+    [EmailListCompleted] BIT,
+    [EmailItemCompleted] BIT,
+    [EmailInvitation] BIT
 )
 GO
 
